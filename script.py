fa2e25844a90ee853fc6bd07fb6f535df35804ff
Joerie <joerie_svd@hotmail.com>
2019-01-19 18:17:15 +0100

    Add indexation

--- script.py
+++ script.py
@@ -14,8 +14,27 @@ print(get_destination_index("Paris, France"))
 
 def get_traveler_location(traveler):
     traveler_destination = traveler[1]
-    traveler_destination_index =get_destination_index(traveler_destination)
+    traveler_destination_index = get_destination_index(traveler_destination)
     return traveler_destination_index
+
 
 test_destination_index = get_traveler_location(test_traveler)
 print(test_destination_index)
+
+
+attractions = []
+for place in range(5):
+    attractions.append([])
+print(attractions)
+
+
+def add_attraction(destination):
+    try:
+        destination_index = get_destination_index(destination)
+        return destination_index
+    except IndexError:
+        print("That destination doesn't exist!")
+
+
+
+print(add_attraction("Pari France"))
